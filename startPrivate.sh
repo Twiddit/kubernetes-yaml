747c048d9307d772edd203d1b174f7973379e3ea
joviedog <joviedog@unal.edu.co>
2023-06-16 20:48:15 -0500

    Updating routing

--- startPrivate.sh
+++ startPrivate.sh
@@ -2,17 +2,17 @@
 echo "[X] Iniciando Cluster Privado"
 
 # Iniciar los volumenes 
-kubectl apply -f kubernetes-yaml/twiddit-volumes/twiddit-volumes.yaml
+kubectl apply -f twiddit-volumes/twiddit-volumes.yaml
 
 # Iniciar el LDAP 
-kubectl apply -f kubernetes-yaml/twiddit-ldap/twiddit-ldap-service.yaml
-kubectl apply -f kubernetes-yaml/twiddit-ldap/twiddit-ldap-deployment.yaml
+kubectl apply -f twiddit-ldap/twiddit-ldap-service.yaml
+kubectl apply -f twiddit-ldap/twiddit-ldap-deployment.yaml
 
-kubectl apply -f kubernetes-yaml/twiddit-ldap/phpldapadmin-service.yaml
-kubectl apply -f kubernetes-yaml/twiddit-ldap/phpldapadmin-deployment.yaml
+kubectl apply -f twiddit-ldap/phpldapadmin-service.yaml
+kubectl apply -f twiddit-ldap/phpldapadmin-deployment.yaml
 
 # Iniciar RabbitMQ 
-kubectl apply -f kubernetes-yaml/twiddit-ldap/phpldapadmin-service.yaml
-kubectl apply -f kubernetes-yaml/twiddit-ldap/phpldapadmin-deployment.yaml
+kubectl apply -f twiddit-ldap/phpldapadmin-service.yaml
+kubectl apply -f twiddit-ldap/phpldapadmin-deployment.yaml
 
 
