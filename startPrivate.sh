ac758197d5002c16c202603d8274e3674adfb752
joviedog <joviedog@unal.edu.co>
2023-06-16 20:34:01 -0500

    First commit

---
+++ startPrivate.sh
@@ -0,0 +1,18 @@
+# Inicio Cluster Privado Twiddit
+
+
+# Iniciar los volumenes 
+kubectl apply -f kubernetes-yaml/twiddit-volumes/twiddit-volumes.yaml
+
+# Iniciar el LDAP 
+kubectl apply -f kubernetes-yaml/twiddit-ldap/twiddit-ldap-service.yaml
+kubectl apply -f kubernetes-yaml/twiddit-ldap/twiddit-ldap-deployment.yaml
+
+kubectl apply -f kubernetes-yaml/twiddit-ldap/phpldapadmin-service.yaml
+kubectl apply -f kubernetes-yaml/twiddit-ldap/phpldapadmin-deployment.yaml
+
+# Iniciar RabbitMQ 
+kubectl apply -f kubernetes-yaml/twiddit-ldap/phpldapadmin-service.yaml
+kubectl apply -f kubernetes-yaml/twiddit-ldap/phpldapadmin-deployment.yaml
+
+
