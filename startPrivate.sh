3053c6cbb90abcb0bde777ba604c130781204a8a
joviedog <joviedog@unal.edu.co>
2023-06-16 22:26:06 -0500

    Adding YAML files

--- startPrivate.sh
+++ startPrivate.sh
@@ -1,18 +1,16 @@
 # Inicio Cluster Privado Twiddit
 echo "[X] Iniciando Cluster Privado"
 
-# Iniciar los volumenes 
-kubectl apply -f twiddit-volumes/twiddit-volumes.yaml
-
 # Iniciar el LDAP 
-# kubectl apply -f twiddit-ldap/twiddit-ldap-service.yaml
-# kubectl apply -f twiddit-ldap/twiddit-ldap-deployment.yaml
+kubectl apply -f twiddit-ldap/twiddit-ldap-service.yaml
+kubectl apply -f twiddit-ldap/twiddit-ldap-deployment.yaml
 
-# kubectl apply -f twiddit-ldap/phpldapadmin-service.yaml
-# kubectl apply -f twiddit-ldap/phpldapadmin-deployment.yaml
+kubectl apply -f twiddit-ldap/phpldapadmin-service.yaml
+kubectl apply -f twiddit-ldap/phpldapadmin-deployment.yaml
 
 # Iniciar RabbitMQ 
-# kubectl apply -f twiddit-mq/rabbitmq-service.yaml
-# kubectl apply -f twiddit-mq/rabbitmq-deployment.yaml
+kubectl apply -f twiddit-mq/rabbitmq-service.yaml
+kubectl apply -f twiddit-mq/rabbitmq-deployment.yaml
 
-
+# Iniciar los volumenes 
+kubectl apply -f twiddit-volumes/twiddit-volumes.yaml
