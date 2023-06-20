4b1e82029d164cd6e28b34259fc059078f97031b
Juan Esteban Oviedo <joviedog@unal.edu.co>
2023-06-19 19:44:59 -0500

    Updating parameter for scaling

--- scalePrivate.sh
+++ scalePrivate.sh
@@ -1,9 +1,9 @@
-echo "[X] Escalando servicios de red privada a 2 replicas"
+echo "[X] Escalando servicios de red privada a $1 replicas"
 
-kubectl scale deployment auth-ms --replicas 2
-kubectl scale deployment twiddit-profile --replicas 2
-kubectl scale deployment twiddit-search-service --replicas 2
-kubectl scale deployment twiddit-social --replicas 2
-kubectl scale deployment twiddit-twiddits --replicas 2
-kubectl scale deployment twiddit-wa --replicas 2
-kubectl scale deployment twiddit-ag --replicas 2
+kubectl scale deployment auth-ms --replicas $1
+kubectl scale deployment twiddit-profile --replicas $1
+kubectl scale deployment twiddit-search-service --replicas $1
+kubectl scale deployment twiddit-social --replicas $1
+kubectl scale deployment twiddit-twiddits --replicas $1
+kubectl scale deployment twiddit-wa --replicas $1
+kubectl scale deployment twiddit-ag --replicas $1
