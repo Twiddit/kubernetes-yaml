f24825fb1537649a9490d6d8c93ece718402ea44
Juan Esteban Oviedo <joviedog@unal.edu.co>
2023-06-23 13:44:35 -0500

    Update scalePrivate.sh

--- scalePrivate.sh
+++ scalePrivate.sh
@@ -2,7 +2,7 @@ echo "[X] Escalando servicios de red privada a $1 replicas"
 
 kubectl scale deployment auth-ms --replicas $1
 kubectl scale deployment twiddit-profile --replicas $1
-# kubectl scale deployment twiddit-search-service --replicas $1
+kubectl scale deployment twiddit-search-service --replicas $1
 kubectl scale deployment twiddit-social --replicas $1
 kubectl scale deployment twiddit-twiddits --replicas $1
 kubectl scale deployment twiddit-wa --replicas $1
